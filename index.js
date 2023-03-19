6117611bfe8cd1c5013095a7bbf7482f7bcecf07
Malek-Abdelal <malekbassam1144@gmail.com>
2023-03-19 19:50:39 +0300

    Firs version of Movie app

--- index.js
+++ index.js
@@ -9,29 +9,38 @@ app.listen(port, () => {
     console.log("Welcome, this is my first server");
 })
 
+function Movie (title, posterPath, overview){
+    this.title = title;
+    this.posterPath = posterPath;
+    this.overview = overview;
+}
+
+function ErrorHandler(status, responseText){
+    this.status = status;
+    this.responseText = responseText;
+}
+
 app.get('/', homeHandler);
 
 function homeHandler (req, res) {
-    let newMovie = new Movie(movieData); 
-    res.json(newMovie.movieObj);
+    let newMovie = new Movie(movieData.title, movieData.poster_path, movieData.overview); 
+    res.json(newMovie);
 }
 
 app.get("/favorite", favoriteHandler);
-
-function Movie (movieObj){
-    this.movieObj = movieObj;
-}
 
 function favoriteHandler(req, res){
     res.send("Welcome to Favorite Page");
 }
 
 function error404Handler(req, res){
-    res.status(404).send("page not found");
+    let error = new ErrorHandler(404, "page not found");
+    res.status(404).send(error);
 }
 
 function error500Handler(req, res){
-    res.status(500).send("Sorry, something went wrong");
+    let error = new ErrorHandler(500, "Sorry, something went wrong");
+    res.status(500).send(error);
 }
 
 app.all('*', error404Handler);
